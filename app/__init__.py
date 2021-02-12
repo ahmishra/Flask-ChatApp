6575e55f75a0d4d756fbd3442b9a7ad66227e61e
ahmishra <umasaryan@gmail.com>
2021-02-12 09:25:48 +0530

    Package Structure Fixed, Ready For Local Use

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
 from flask import Flask
 from flask_socketio import SocketIO
+import os
 
 socketio = SocketIO()
 
 
 def create_app(debug=False):
     """Create an application."""
+
+    # SETTING THE ENV VARIABLES
+    os.environ['HOST'] = '192.168.1.1'
+    os.environ['PORT'] = '5000'
+    os.environ['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+
+
+
     app = Flask(__name__)
     app.debug = debug
-    app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     socketio.init_app(app)
     return app
-
--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 from app import create_app, socketio
+import os
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host=os.getenv("HOST"), port=os.getenv("PORT"))
